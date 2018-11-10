92af1da2c4a46128b56f83eb02acbf5868b7a95d
Sergeygeek <sukhorukov1986@yandex.ru>
2018-11-10 22:59:06 +0400

    добавил делет

--- image.php
+++ image.php
@@ -12,7 +12,7 @@ if(!empty($_GET['action']) && ($_GET['action'] == 'showImg') && !empty($_GET['id
     $row = mysqli_fetch_assoc($res);
     $src = $row['fullSrc'];
     $count = $row['count'];
-    $sql = "SELECT name, feedback FROM feedback WHERE id_image = " . $idImg;
+    $sql = "SELECT id, name, feedback FROM feedback WHERE id_image = " . $idImg;
     $res = mysqli_query($link, $sql);
     $contentFeedback = '';
         while ($row = mysqli_fetch_assoc($res)){
@@ -20,6 +20,7 @@ if(!empty($_GET['action']) && ($_GET['action'] == 'showImg') && !empty($_GET['id
         <div class="feedback">
             <div class=\"user_name\">'.$row['name'].'</div>
             <div class=\"user_feedback\">'.$row['feedback'].'</div>
+            <a href="image.php?action=showImg&id='.$idImg.'&id_feed='.$row['id'].'&add=delFeedback">X</a>
         </div>';
         }
 }
@@ -31,6 +32,14 @@ if($_GET['add'] == 'addFeedback'){
     $sql = "INSERT INTO feedback (id_image, name, feedback) VALUES ('$idImg', '$name', '$feedback')";
     $res = mysqli_query($link, $sql);
     header("location: image.php?action=showImg&id=$idImg");
+    exit;
+}
+
+if($_GET['add'] == 'delFeedback'){
+    $sql = "DELETE FROM feedback WHERE id = " . $_GET['id_feed'];
+    $res = mysqli_query($link, $sql);
+    header("location: image.php?action=showImg&id=$idImg");
+    exit;
 }
 ?>
 
